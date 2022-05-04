b10210f87862517f04d72ce937ce668ced54f66e
Lighty <kh.ultra1234@gmail.com>
2022-05-04 12:43:59 +0300

    Update plugins.js

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,46 +1,108 @@
+var controller = new ScrollMagic.Controller();
 
-
-anime({
-     targets: '.TERADEK_LOWER_THIRD_ANIMATION_32 .scene-1 .scene-1__content .content__group-1',
-     width:[0+'%',100+'%'],
-     easing:'easeInOutExpo',
-     duration:1000,
-     delay:500,
+var animation = anime({
+    targets: ".element",
+    scale: [1.5]
 });
-anime({
-     targets: '.TERADEK_LOWER_THIRD_ANIMATION_32 .scene-1 .scene-1__content .content__group-1 .content__left--decoration',
-     left:[0+'%',100+'%'],
-     easing:'easeInOutQuad',
-     duration:1200,
-     delay:650,
-     complete: () => {
-          anime({
-               targets: '.TERADEK_LOWER_THIRD_ANIMATION_32 .scene-1 .scene-1__content .content__group-1 .content__left--decoration',
-               easing:'linear',
-               duration:600,
-               left: [
-                    { value: -20+'%', duration: 1 },
-                    { value: 20+'%', duration: 500, delay: 100 },
-                    { value: -8+'%', duration: 500, delay: 100 },
-                    { value: 2+'%', duration: 500, delay: 100 },
-                    { value: -25+'%', duration: 500, delay: 200 },
-                    { value: 100+'%', duration: 1, delay: 50 }
-               ],
-               complete:() => {
-                    anime({
-                         targets: '.TERADEK_LOWER_THIRD_ANIMATION_32 .scene-1 .scene-1__content .content__group-1',
-                         width:[100+'%',0+'%'],
-                         easing:'easeInOutExpo',
-                         duration:1000,
-                         delay:250,
-                    });
-                    anime({
-                         targets: '.TERADEK_LOWER_THIRD_ANIMATION_32 .scene-1 .scene-1__content .content__group-1 .content__left--decoration',
-                         left:[100+'%',0+'%'],
-                         easing:'linear',
-                         duration:1000,
-                    });
+
+new ScrollMagic.Scene({
+    triggerElement: '.trigger',
+    duration: "130%",
+    triggerHook: 0.3
+})
+     .setAnime(animation)
+     .addIndicators()
+     .addTo(controller);
+
+
+// observer on scroll 
+
+//можно делать отслеживание анимаций двух форматов просто через if или если нужно в обе стороны то через else
+function ups() {
+     anime({
+          targets: '.detection',
+          easing: 'linear',
+          duration: 2500,
+          translateY: 250
+     });
+}
+(function () {
+     const images = document.querySelectorAll('.detection');
+
+   let  observer = new IntersectionObserver((entries) => {
+
+          entries.forEach(entry => {
+               if(entry.intersectionRatio > 0) {
+                         entry.target.style.backgroundColor = '#000';
+                         ups();
                }
-          });
+               else {
+                    entry.target.style.backgroundColor = '#eaea';
+               }
+          })
      }
+
+     });
+
+     images.forEach(image => {
+          observer.observe(image)
+     });
+})();
+
+//use but have some problem with vh , two ways observe
+
+$( document ).ready(function() {
+     $(window).scroll(function() {
+     let square = document.querySelector('.header__square .section-number');
+     let leftLine = document.querySelector('.decoration__text');
+     let arrowTop = document.querySelector('.arrow-top a');
+     let arrowBottom = document.querySelector('.arrow-bottom a');
+
+          var hT = $('#wealth').offset().top,
+               hH = $('#wealth').outerHeight(),
+               wH = $(window).height(),
+               wS = $(this).scrollTop();
+          if (wS > (hT+(hH-50)-wH) && (hT > wS) && (wS+wH > hT+(hH-50))){
+               square.innerText = '02';
+          leftLine.innerHTML ='OUR GOAL';
+          arrowTop.href = "#partners";
+          arrowBottom.href = "#strong";
+          }
+     });
 });
+
+// and last
+
+$(window).scroll(function() {
+     $('.detection').each(function(){
+          var imagePos = $(this).offset().top;
+          var topOfWindow = $(window).scrollTop();
+          if (imagePos < topOfWindow+600) {
+               console.log(this);
+          }
+          else   {
+
+               console.log('1234');
+          }
+     });
+});
+
+//////
+
+function stop() {
+     window.addEventListener("load", function() {
+          window.addEventListener('scroll', () => {
+
+               var d = document.querySelectorAll('.detection');
+               d.forEach(function (elem ) {
+                    var imagePos = d.offsetTop;
+                    var topOfWindow = window.scrollTop;
+                    if (imagePos < topOfWindow+600) {
+                         console.log(elem);
+                    }
+
+               });
+          });
+     });
+
+}stop();
